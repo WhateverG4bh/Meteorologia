4bdbe4eae1c521591f5ac4b11e3598bb5d437c69
Antonio Gabriel Nunes Marques <iegaton786@gmail.com>
2024-09-09 17:51:33 -0300

    Update at.js

--- js/at.js
+++ js/at.js
@@ -9,7 +9,6 @@ document.getElementById('searchButton').addEventListener('click', function() {
     }
 });
 
-
 function fetchWeather(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`)
         .then(response => {
@@ -33,10 +32,43 @@ function displayWeather(data) {
     const weatherDescription = data.weather[0].description;
     const humidity = 'Umidade: ' + data.main.humidity + '%';
     const weatherIcon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    
+    const backgroundElement = document.querySelector('.background');
+    
+    let backgroundImage = '';
+    let animationClass = '';
+
+    switch (data.weather[0].main) {
+        case 'Clear':
+            backgroundImage = 'url("https://t3.ftcdn.net/jpg/05/91/10/70/360_F_591107051_Lmfn0UW7Gd2Xq6gks7Gf8JLAhKNXO0gJ.jpg")';
+
+            break;
+        case 'Rain':
+            backgroundImage = 'url("https://images.ecycle.com.br/wp-content/uploads/2023/11/08112514/pexels-ave-calvar-martinez-3497624-scaled.jpg.webp")';
+
+            break;
+        case 'Clouds':
+            backgroundImage = 'url("https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+
+            break;
+        case 'Snow':
+            backgroundImage = 'url("https://annphoto.net/wp-content/uploads/2021/02/13-dicas-para-fotografar-neve-um-guia-para-iniciantes.jpgfit750536ssl1-750x470.jpeg")';
+
+            break;
+        case 'Thunderstorm':
+            backgroundImage = 'url("https://www.worldatlas.com/upload/1b/40/c1/shutterstock-1906271014.jpg")';
+
+            break;
+        default:
+            backgroundImage = 'url("https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/9baa4d787ac9b638b839530ae7180f70.jpg")';
+    }
 
     document.getElementById('cityName').innerText = `Tempo ${cityName}`;
     document.getElementById('temperature').innerText = temperature;
     document.getElementById('weatherDescription').innerText = weatherDescription;
     document.getElementById('humidity').innerText = humidity;
     document.getElementById('weatherIcon').innerHTML = `<img src="${weatherIcon}" alt="Weather Icon">`;
+
+    backgroundElement.style.backgroundImage = backgroundImage;
+    backgroundElement.className = `background ${animationClass}`;
 }
